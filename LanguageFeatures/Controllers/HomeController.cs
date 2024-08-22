31dfb926c8a45aae1f17a7fc827f7a2b6ef8f0bb
Oliver Sokol <oliver.sokol@nhi.no>
2024-08-22 13:58:30 +0200

    starting chapter 5

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product?[] products = Product.GetProducts();
+
+
+            // Null forgiving operator assures is not null
+            //return View(new string[] { products[0]!.Name });
+            //return View(new string[] { $"Name: {products[0]?.Name}, Price: {products[0]?.Price:C2}" });
+
+            return View("Index", new string[] { "bob", "joe", "Alice" });
+        }
+    }
+}
