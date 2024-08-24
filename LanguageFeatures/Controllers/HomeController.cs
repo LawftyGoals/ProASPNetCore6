769b535c4ffdeeb2ed06f6fe7552cdfb1cfa1572
Oliver Sokol <oliver.sokol@nhi.no>
2024-08-25 01:44:07 +0200

    chapter5

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
 
 namespace LanguageFeatures.Controllers
 {
@@ -6,14 +7,61 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            Product?[] products = Product.GetProducts();
+            //Product?[] products = Product.GetProducts();
 
 
             // Null forgiving operator assures is not null
             //return View(new string[] { products[0]!.Name });
             //return View(new string[] { $"Name: {products[0]?.Name}, Price: {products[0]?.Price:C2}" });
 
-            return View("Index", new string[] { "bob", "joe", "Alice" });
+            //return View("Index", new string[] { "bob", "joe", "Alice" });
+
+            //Dictionary<string, Product> products = new Dictionary<string, Product> { { "Kayak", new Product { Name = "Kayak", Price = 275M } }, { "Lifejacket", new Product { Name = "Lifejacket", Price = 48.95M } } };
+            //Dictionary<string, Product> products = new Dictionary<string, Product> { ["Kayak"] = new Product { Name = "Kayak", Price = 275M }, ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M } };
+
+            //Target Typed
+            //Dictionary<string, Product> products = new () { ["Kayak"] = new Product { Name = "Kayak", Price = 275M }, ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M } };
+            //
+            //return View("Index", products.Keys);
+
+            //object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
+            //decimal total = 0;
+            //for (int i = 0; i < data.Length; i++)
+            //{
+            //    if (data[i] is decimal d)
+            //    {
+            //        total += d;
+            //    }
+            //}
+            //
+            //total = 0;
+            //foreach (var dat in data)
+            //{
+            //    if (dat is decimal d)
+            //    {
+            //        total += d;
+            //    }
+            //}
+            //
+            //total = 0;
+            //
+            //foreach (var dat in data)
+            //{
+            //    switch (dat)
+            //    {
+            //        case decimal dv:
+            //            total += dv;
+            //            break;
+            //        case int iv when iv > 50:
+            //            total += iv;
+            //            break;
+            //    };
+            //}
+
+            ShoppingCart cart = new() { Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Index", new string[] { $"Total: {cartTotal:C2}" });
         }
     }
 }
---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            if (cartParam.Products != null)
+            {
+                foreach(Product? prod in cartParam.Products)
+                {
+                    total += prod?.Price ?? 0;
+                }
+            }
+            return total;
+        }
+    }
+}
